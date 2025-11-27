c29fe2dd1e6873166899beb556374a47a7dc02f5
Davi <davvisamuelsc@gmail.com>
2025-11-26 22:47:15 -0300

    Commit 17

--- src/HtmlStatement.java
+++ src/HtmlStatement.java
@@ -1,25 +1,23 @@
 package src;
 
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
     @Override
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += each.getMovie().getTitle() + ": " +
-                    String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        // add footer lines
-        result += "<P>You owe <EM>" +
-                String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" +
+    protected String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+    }
+
+    @Override
+    protected String eachRentalString(Rental aRental) {
+        return aRental.getMovie().getTitle() + ": " +
+                String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+
+    @Override
+    protected String footerString(Customer aCustomer) {
+        return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
+                "On this rental you earned <EM>" +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
                 "</EM> frequent renter points<P>";
-        return result;
     }
 }
--- src/Statement.java
+++ src/Statement.java
@@ -1,5 +1,23 @@
 package src;
 
+import java.util.Enumeration;
+
 public abstract class Statement {
-    public abstract String value(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerString(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += eachRentalString(each);
+        }
+        result += footerString(aCustomer);
+        return result;
+    }
+
+    protected abstract String headerString(Customer aCustomer);
+
+    protected abstract String eachRentalString(Rental aRental);
+
+    protected abstract String footerString(Customer aCustomer);
 }

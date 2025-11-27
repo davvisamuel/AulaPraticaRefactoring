d83aff86057de13b41be24349fc74530f77af893
Davi <davvisamuelsc@gmail.com>
2025-11-26 22:27:48 -0300

    Commit 11

--- src/Movie.java
+++ src/Movie.java
@@ -6,19 +6,31 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -26,34 +38,33 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
 
-        switch (_priceCode) {
+        switch (getPriceCode()) {
 
             case REGULAR:
-                result += 2;
+                double result = 2;
                 if (daysRented > 2) {
                     result += (daysRented - 2) * 1.5;
                 }
-                break;
+                return result;
 
             case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
+                return daysRented * 3;
 
             case CHILDRENS:
-                result += 1.5;
+                double resultChild = 1.5;
                 if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
+                    resultChild += (daysRented - 3) * 1.5;
                 }
-                break;
-        }
+                return resultChild;
 
-        return result;
+            default:
+                throw new IllegalArgumentException("Invalid Price Code in getCharge()");
+        }
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (_priceCode == Movie.NEW_RELEASE && daysRented > 1) {
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
             return 2;
         }
         return 1;
---
+++ src/Price.java
@@ -0,0 +1,6 @@
+package src;
+
+public abstract class Price {
+    public abstract int getPriceCode();
+}
+

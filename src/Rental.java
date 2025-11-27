768edcc8648278f79e85856d1b0a39c0098c0810
Davi <davvisamuelsc@gmail.com>
2025-11-26 22:17:09 -0300

    Commit 4

--- src/Rental.java
+++ src/Rental.java
@@ -10,6 +10,32 @@ public class Rental {
         _daysRented = daysRented;
     }
 
+    public double getCharge() {
+        double thisAmount = 0;
+
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (this.getDaysRented() > 2) {
+                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += this.getDaysRented() * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (this.getDaysRented() > 3) {
+                    thisAmount += (this.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        return thisAmount;
+    }
+
     public int getDaysRented() {
         return _daysRented;
     }
